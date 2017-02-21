93511bb0665c06b55d7d7750ce61d7c491284ad1
Cesar Rodriguez <carr235@gmail.com>
2017-02-21 11:05:34 -0430

    commir

---
+++ Back Office/Dominio/Entidades/Promocion.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio.Entidades
+{
+    public class Promocion : Entidad
+    {
+         #region Atributos
+        private string nombre;
+        private string imagen;
+        private int activo;
+        private DateTime fecha;
+
+        #endregion
+
+        #region Get's Set's
+
+
+        public string Nombre
+        {
+            get { return nombre; }
+           
+        }
+
+        public string Imagen
+        {
+            get { return imagen; }
+            
+        }
+
+        public int Activo
+        {
+            get { return activo; }
+            
+        }        
+
+        public DateTime Fecha
+        {
+            get { return fecha; }
+            
+        }
+
+        
+        #endregion
+
+        #region Constructores
+
+        public Promocion()
+        {
+            Id = 0;
+            nombre = String.Empty;
+            imagen = String.Empty;
+            activo = 0;
+            fecha = DateTime.Now;
+        }
+      
+        #endregion
+    }
+}
