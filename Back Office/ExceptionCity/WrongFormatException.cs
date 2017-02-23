57e2f7d21feb6f4eff512ac31a4a49ecc02fea94
Cesar Rodriguez <carr235@gmail.com>
2017-02-23 17:25:40 -0400

    commit

---
+++ Back Office/ExceptionCity/WrongFormatException.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionCity
+{
+    public class WrongFormatException : ExceptionsCity
+    {
+        public WrongFormatException()
+            : base()
+        { }
+
+        public WrongFormatException(string message)
+            : base(message)
+        {
+        }
+
+        public WrongFormatException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+
+        public WrongFormatException(string codigo, string message, Exception inner)
+            : base(codigo, message, inner)
+        {
+        }
+    }
+}
---
+++ Back Office/LogicaCC/Comandos/ComandoAgregarCategoria.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DatosCC.Categoria;
+using DatosCC.Fabrica;
+using DatosCC.InterfazDAO;
+using Dominio;
+using ExceptionCity;
+
+namespace LogicaCC.Comandos
+{
+    public class ComandoAgregarCategoria : Comando<bool>
+    {
+        /// <summary>
+        /// Constructor del comando
+        /// </summary>
+        /// <param name="parametro">Factura a categoria</param>
+        public ComandoAgregarCategoria(Entidad parametro)
+        {
+            LaEntidad = parametro;
+        }
+
+        /// <summary>
+        /// Metodo que ejecuta el comando
+        /// </summary>
+        /// <returns>booleano que refleja el exito de la ejecucion del comando</returns>
+        public override bool Ejecutar()
+        {
+            try
+            {                
+                IDao daoCategoria = FabricaDAOSqlServer.crearDaoCategoria();
+                bool respuesta = daoCategoria.Agregar(this.LaEntidad);
+                return respuesta;
+            }
+            catch (ArgumentNullException ex)
+            {
+
+                throw new NullArgumentException(ResourcesLogic.Codigo, ResourcesLogic.Mensaje, ex);
+            }
+            catch (FormatException ex)
+            {
+                throw new WrongFormatException(ResourcesLogic.Codigo, ResourcesLogic.Mensaje_Error_Formato, ex);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }             
+        }
+    }
+}
